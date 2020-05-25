0033247631d7a10df3ba8e8585f7d50b5d396982
Kyle <kyledc0804@hotmail.com>
2020-05-24 21:57:26 -0700

    Added CameraController and PlayerLoader
    Camera now follows player and is bounded by scene size
    PlayerLoader created to always instantiate a Player object

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] Transform targetObject;
+
+    [SerializeField] private Tilemap tileMap;
+    private Vector3 bottomLeftLimit;
+    private Vector3 topRightLimit;
+
+    private float halfHeight;
+    private float halfWidth;
+
+    private Vector3 bottomLeftAspectRatio;
+    private Vector3 topRightAspectRatio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Set to Player obj
+        targetObject = PlayerController.instance.transform;
+
+        // Get current ratio of Camera
+        this.CalculateCameraClamp();
+
+        // Set Camera Limits when moving camera
+        bottomLeftLimit = tileMap.localBounds.min + bottomLeftAspectRatio;
+        topRightLimit = tileMap.localBounds.max + topRightAspectRatio; 
+    }
+
+    // LateUpdate is called once per frame after Update
+    void LateUpdate()
+    {
+        // Move Camera based on player position
+        transform.position = new Vector3(targetObject.position.x, targetObject.position.y, transform.position.z);
+
+        // Keep Camera within tilemap bounds
+        float xCameraClamp = Mathf.Clamp(transform.position.x, bottomLeftLimit.x, topRightLimit.x);
+        float yCameraClamp = Mathf.Clamp(transform.position.y, bottomLeftLimit.y, topRightLimit.y);
+        transform.position = new Vector3(xCameraClamp, yCameraClamp, transform.position.z);
+    }
+
+    private void CalculateCameraClamp()
+    {
+        halfHeight = Camera.main.orthographicSize;
+        halfWidth = halfHeight * Camera.main.aspect;
+        bottomLeftAspectRatio = new Vector3(halfWidth, halfHeight, 0f);
+        topRightAspectRatio = new Vector3(-halfWidth, -halfHeight, 0f);
+    }
+}
---
+++ Assets/Scripts/PlayerLoader.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLoader : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Verify that a player exists per scene
+        if (PlayerController.instance == null)
+        {
+            Instantiate(player);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
