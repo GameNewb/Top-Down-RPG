679dc550cd3d81d1c582e817f3f86db17d6b40a8
Kyle <kyledc0804@hotmail.com>
2020-09-05 20:08:55 -0700

    Added UI Icons
    Added some UI icons for Main Menu and Player menu

---
+++ Assets/Scripts/UI Scripts/MouseOverControl.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class MouseOverControl : MonoBehaviour, IPointerEnterHandler
+{
+    [SerializeField] private GameObject menuCursor;
+    
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Vector3 newTransform = new Vector3(gameObject.transform.position.x - 130f, gameObject.transform.position.y - 10f, gameObject.transform.position.z);
+        menuCursor.transform.position = newTransform;
+    }
+
+}
