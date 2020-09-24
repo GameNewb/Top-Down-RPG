991fa3b2cb6eb4659b414412c566d842bb8e6150
Kyle <kyledc0804@hotmail.com>
2020-09-23 20:36:43 -0700

    Added UI changes to the stat buttons
    Activate/inactivate buttons if stats points is empty
    TODO: Re-enable add buttons for all if theres stat points

--- Assets/Scripts/Player Scripts/StatManager.cs
+++ Assets/Scripts/Player Scripts/StatManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StatManager : MonoBehaviour
 {
@@ -8,36 +9,68 @@ public class StatManager : MonoBehaviour
     string statName;
     public int selectedCharacter = 0;
 
+    [SerializeField] private GameObject subtractButton;
+    [SerializeField] private GameObject addButton;
+
     private void Start()
     {
         playerStats = GameManager.instance.playerStats;
         statName = transform.name.ToLower();
+
+        if (playerStats[selectedCharacter].statPoints <= 0)
+        {
+            addButton.GetComponent<Button>().interactable = false;
+        }
+        else
+        {
+            addButton.GetComponent<Button>().interactable = true;
+        }
     }
 
     // Function to add to the current stat
     public void AddStat()
     {
-        switch (statName)
+        // Only allow add when character has enough stat points
+        if (playerStats[selectedCharacter].statPoints > 0)
         {
-            case "strength":
-                playerStats[selectedCharacter].strength++;
-                break;
-            case "vitality":
-                playerStats[selectedCharacter].vitality++;
-                break;
-            case "intelligence":
-                playerStats[selectedCharacter].intelligence++;
-                break;
-            case "dexterity":
-                playerStats[selectedCharacter].dexterity++;
-                break;
-            case "luck":
-                playerStats[selectedCharacter].luck++;
-                break;
+            addButton.GetComponent<Button>().interactable = true;
+
+            switch (statName)
+            {
+                case "strength":
+                    playerStats[selectedCharacter].strength++;
+                    break;
+                case "vitality":
+                    playerStats[selectedCharacter].vitality++;
+                    break;
+                case "intelligence":
+                    playerStats[selectedCharacter].intelligence++;
+                    break;
+                case "dexterity":
+                    playerStats[selectedCharacter].dexterity++;
+                    break;
+                case "luck":
+                    playerStats[selectedCharacter].luck++;
+                    break;
+            }
+
+            playerStats[selectedCharacter].statPoints--;
+
+            if (playerStats[selectedCharacter].statPoints <= 0)
+            {
+                addButton.GetComponent<Button>().interactable = false;
+            }
+
+            // Update UI
+            GameMenu.instance.UpdatDetailedStat(selectedCharacter);
+        } 
+        else
+        {
+            addButton.GetComponent<Button>().interactable = false;
         }
 
-        // Update UI
-        GameMenu.instance.UpdatDetailedStat(selectedCharacter);
+        // Reactivate subtract button
+        subtractButton.GetComponent<Button>().interactable = true;
     }
 
     // Function to subtract from the current stat
@@ -47,20 +80,49 @@ public class StatManager : MonoBehaviour
         {
             case "strength":
                 playerStats[selectedCharacter].strength--;
+                if (playerStats[selectedCharacter].strength <= 0) {
+                    playerStats[selectedCharacter].strength = 0;
+                    subtractButton.GetComponent<Button>().interactable = false;
+                }
                 break;
+
             case "vitality":
                 playerStats[selectedCharacter].vitality--;
+                if (playerStats[selectedCharacter].vitality <= 0) {
+                    playerStats[selectedCharacter].vitality = 0;
+                    subtractButton.GetComponent<Button>().interactable = false;
+                }
                 break;
+
             case "intelligence":
                 playerStats[selectedCharacter].intelligence--;
+                if (playerStats[selectedCharacter].intelligence <= 0) {
+                    playerStats[selectedCharacter].intelligence = 0;
+                    subtractButton.GetComponent<Button>().interactable = false;
+                }
                 break;
+
             case "dexterity":
                 playerStats[selectedCharacter].dexterity--;
+                if (playerStats[selectedCharacter].dexterity <= 0) {
+                    playerStats[selectedCharacter].dexterity = 0;
+                    subtractButton.GetComponent<Button>().interactable = false;
+                }
                 break;
+
             case "luck":
                 playerStats[selectedCharacter].luck--;
+                if (playerStats[selectedCharacter].luck <= 0) {
+                    playerStats[selectedCharacter].luck = 0;
+                    subtractButton.GetComponent<Button>().interactable = false;
+                }
                 break;
         }
+        
+        playerStats[selectedCharacter].statPoints++;
+
+        // Reactivate add button
+        addButton.GetComponent<Button>().interactable = true;
 
         // Update UI
         GameMenu.instance.UpdatDetailedStat(selectedCharacter);
